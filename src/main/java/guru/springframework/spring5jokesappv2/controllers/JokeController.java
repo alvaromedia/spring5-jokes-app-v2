ef629598c31dcb343abad6ca35d39fe3b6092563
alvaro <alvarorajic2@gmail.com>
2023-01-26 19:46:34 +0100

    created template (added View). closes #5

--- src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -2,11 +2,11 @@ package guru.springframework.spring5jokesappv2.controllers;
 
 import guru.springframework.spring5jokesappv2.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
+@Controller
 public class JokeController {
 
     private final JokeService jokeService;
